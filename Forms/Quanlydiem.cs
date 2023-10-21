c90bcb2e3258b4952bee397904bd6509ac1a9a4c
Ducdev710 <ducvuminh0710@gmail.com>
2023-10-21 22:43:46 +0700

    ko thay doi gi het

--- Forms/Quanlydiem.cs
+++ Forms/Quanlydiem.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyDiemSinhVien.Forms
 {
-	public partial class Form2 : Form
+	public partial class Quanlydiem : Form
 	{
-		public Form2()
+		public Quanlydiem()
 		{
 			InitializeComponent();
 		}
